296cca4941644c070acf1435218c4afb0cf06a7f
iAmSherm2 <trace.anderson@students.fhu.edu>
2024-02-16 19:19:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def push(self, card):
-        pass
+        self.cards.append(card)
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        pass
+        del self.cards[0]
 
 if __name__ == '__main__':
-    #REMOVE PASS AND YOUR CODE GOES HERE
     pass
